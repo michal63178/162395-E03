dab0543acb65d738c4e54425d2d623b1309f0287
Michał Jakubowski <michal.jakubowski.730@gmail.com>
2022-02-18 12:19:17 +0100

    Zadanie trzecie

--- Z3/Z3.java
+++ Z3/Z3.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Z3 {
+    static String listFilesWithoutGivenExtension(String path,
+                                                 String extension) {
+        File[] entities = new File(path).listFiles();
+        List<String> files = new ArrayList<>();
+
+        if (entities != null)
+            Arrays.stream(entities)
+                    .forEach(file -> {
+                        if (!(file.isDirectory() || file
+                                .getName()
+                                .endsWith("." + extension)))
+                            files.add(file.getName());
+
+                    });
+
+        return String.join("\n", files);
+
+    }
+
     public static void main(String[] args) {
-        System.out.print("Z3");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj ścieżkę:");
+        String path = input.nextLine();
+        System.out.println();
+        System.out.println("Podaj rozszerzenie:");
+        String extension = input.nextLine();
+
+        System.out.print(listFilesWithoutGivenExtension(
+                path, extension));
 
     }
 }
